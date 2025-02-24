113ca69952501dc798df247c49e99399233000a8
YOSTI ALEXIS ALEJO CONDORI <116779020+Dark-kyo@users.noreply.github.com>
2025-02-23 21:47:13 -0400

    Add files via upload

---
+++ Practica1/Punto.java/Punto.java
@@ -0,0 +1,22 @@
+
+public class Punto {
+	public float x;
+	public float y;
+
+	public Punto(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String coord_cartesianas() {
+		return "("+x+","+y+")";
+	}
+	public String coord_polares() {
+		double r= Math.sqrt(x*x+y*y);
+		double theta= Math.atan2(y, x);
+		return "(r: "+r+", θ: "+theta+")";
+	}
+	public String toString() {
+		return "Punto de coordenadas cartesianas"+coord_cartesianas();
+	}	
+}
---
+++ Practica1/Punto.java/principal.java
@@ -0,0 +1,9 @@
+
+public class principal {
+
+	public static void main (String []args) {
+		Punto p= new Punto (3,4);
+		System.out.println(p.toString());
+		System.out.println("Coordenadas polares"+p.coord_polares());		
+	}
+}
